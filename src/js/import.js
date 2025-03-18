3722fc2aad9e8b9708a51f2ae0ec4ea53ca44d06
mushy <157179472+roxcelic@users.noreply.github.com>
2025-03-18 21:16:14 +0000

    Update import.js

--- src/js/import.js
+++ src/js/import.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const darkThemeUrl = "https://crimtane.roxcelic.love/styles.css";
     const lightThemeUrl = "https://crimtane.roxcelic.love/light.css";
     const backgroundUrl = "https://crimtane.roxcelic.love/script.js";
+
+    const background = document.currentScript.getAttribute('background');
     
     const linkElement = document.createElement("link");
     linkElement.rel = "stylesheet";
-    
-    const scriptElement = document.createElement("script");
     
     if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
         linkElement.href = darkThemeUrl;
@@ -14,20 +14,24 @@ document.addEventListener("DOMContentLoaded", function() {
         linkElement.href = lightThemeUrl;
     }
 
-    // setting up the background config
-    scriptElement.src = backgroundUrl
-        scriptElement.setAttribute("speed", 10);
-        scriptElement.setAttribute("loop", 300);
-        scriptElement.setAttribute("amount", 150);
-        scriptElement.setAttribute("distance", 2);
-        scriptElement.setAttribute("penThickness", 1);
-        scriptElement.setAttribute("reset", "false");
-        scriptElement.setAttribute("resetTime", 1000);
-        scriptElement.setAttribute("backgroundColor", "black");
-        scriptElement.setAttribute("penColor", "rgb(105, 0, 46)");
-        scriptElement.setAttribute("center", "false");
-        scriptElement.setAttribute("name", "ah");
-
-    document.head.appendChild(scriptElement);
+    if (background != "false") {
+        const scriptElement = document.createElement("script");
+        // setting up the background config
+        scriptElement.src = backgroundUrl
+            scriptElement.setAttribute("speed", 10);
+            scriptElement.setAttribute("loop", 300);
+            scriptElement.setAttribute("amount", 150);
+            scriptElement.setAttribute("distance", 2);
+            scriptElement.setAttribute("penThickness", 1);
+            scriptElement.setAttribute("reset", "false");
+            scriptElement.setAttribute("resetTime", 1000);
+            scriptElement.setAttribute("backgroundColor", "black");
+            scriptElement.setAttribute("penColor", "rgb(105, 0, 46)");
+            scriptElement.setAttribute("center", "false");
+            scriptElement.setAttribute("name", "ah");
+    
+        document.head.appendChild(scriptElement);
+    }
+    
     document.head.appendChild(linkElement);
 });
